58940f3dea903675a7af47d9bbdb0a5dedbcc42d
unknown <smithinaiden2003@gmail.com>
2021-01-27 19:00:51 -0800

    Updates to Unity Project

---
+++ BasicGame_TileLevel/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    //Cache Component reference
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    BoxCollider2D myFeet;
+
+
+    // Message then Methods
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myFeet = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlthrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 and +1
+        Vector2 playerVelocity = new Vector2(controlthrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+         Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+         myRigidBody.velocity += jumpVelocityToAdd;
+        }
+
+        
+    }
+
+    private void FlipSprite()
+    {
+
+        bool playerHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+
+    }
+
+}
